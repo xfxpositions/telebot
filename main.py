ad9c918c7938008df78bf8588aeffea99a74bc3a
xfxpositions <xfxpositions@gmail.com>
2024-03-16 13:31:25 +0300

    cli updated to new server events

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.transcription import init_deepgram, init_live_transcription, start_audio_server, select_input_device
 from threading import Event  # Make sure you have this import
+import time
 
 
 server_ready_event = Event()  # Create an event
@@ -8,11 +9,13 @@ deepgram = init_deepgram()
 device_index = select_input_device()
 
 # Pass the event to start_audio_server
-stream_url = start_audio_server(port=5001, device_index=device_index, server_ready_event=server_ready_event)
+stream_url = start_audio_server(port=5001, device_index=device_index)
+
+print(f"stream url: {stream_url}")
 
 # Wait for the server to be ready
-server_ready_event.wait()
+# server_ready_event.wait()
 
 # Once the event is set, proceed to start live transcription
-init_live_transcription(deepgram, stream_url=stream_url, language="tr")
+init_live_transcription(deepgram, stream_url=stream_url, language="tr", textbox=None, stop=False)
 
