3bb6ec422edde7ece8c0ff5ffafca279abaa609c
xfxpositions <xfxpositions@gmail.com>
2024-03-18 13:53:53 +0300

    audio server example updated

--- audioserver.py
+++ audioserver.py
@@ -1,15 +1,19 @@
-from utils.transcription import select_input_device, start_audio_server, list_audio_devices
+from utils.transcription import (
+    select_input_device,
+    start_audio_server,
+    list_audio_devices,
+)
 import pyaudio
 
 audio = pyaudio.PyAudio()
 devices = list_audio_devices(audio)
 print(devices)
-device_index = 18
+device_index = select_input_device()
 stream_url = start_audio_server(port=5003, device_index=device_index)
 print("audio server url started")
 
 while True:
-        # Check if the Enter key has been pressed
-        if input() == '':
-            print("Program stopped.")
-            break
+    # Check if the Enter key has been pressed
+    if input() == "":
+        print("Program stopped.")
+        break
